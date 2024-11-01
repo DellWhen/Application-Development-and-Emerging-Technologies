1090ad906bebfd52bc75a322625d6838f8861aaf
DellWhen <tanwhendellm@yahoo.com>
2024-11-01 21:07:06 +0800

    authentication making

--- AppDev-Auth-Crud-Tan/mywebsiteapp/urls.py
+++ AppDev-Auth-Crud-Tan/mywebsiteapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login"),
-    path("register"),
-    path("signup")
+    path("home/", views.home, name='home'),
+    path("",views.login, name='login'),
+    path("logout/", views.logout, name='logout')
+
+    
 ]
--- AppDev-Auth-Crud-Tan/mywebsiteapp/views.py
+++ AppDev-Auth-Crud-Tan/mywebsiteapp/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html', {
+
+    })
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, "Invalid username or password")
+            redirect('login')
+    else:        
+        return render(request, 'login.html', {})
+
+def logout(request):
+    pass
